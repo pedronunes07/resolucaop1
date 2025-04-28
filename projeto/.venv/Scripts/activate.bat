eec8250c5d45db949fa03e8ceacf4d2699bec57c
Pedro nunes <pedromarcosnunes@gmail.com>
2025-04-28 09:01:27 -0300

    trabalho

--- projeto/.venv/Scripts/activate.bat
+++ projeto/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\user\Desktop\prova\resolucaop1-2\projeto\.venv"
+set "VIRTUAL_ENV=C:\Users\user\Desktop\pedro\resolucaop1\projeto\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
