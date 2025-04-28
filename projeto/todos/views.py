08a5bfb70800a9d7d2bd9a0a9350d1e92a523792
Pedro nunes <pedromarcosnunes@gmail.com>
2025-04-28 12:48:00 -0300

    Trabalho 4

--- projeto/todos/views.py
+++ projeto/todos/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, TemplateView
 from .models import Todo
 from django.urls import reverse_lazy
 
-
+# Suas views existentes
 class TodoListView(ListView):
     model = Todo
 
@@ -21,4 +22,23 @@ class TodoDeleteView(DeleteView):
     model = Todo
     success_url = reverse_lazy("todo_list")
 
-# Create your views here.
+# View para a página de serviço
+class TodoServicoView(TemplateView):
+    template_name = "servico.html"  # Defina o template correspondente
+
+# View para a página de login
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Tenta autenticar o usuário
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)  # Realiza o login do usuário
+            return redirect('todo_servico')  # Redireciona para a página de serviços após o login
+        else:
+            # Caso falhe a autenticação
+            return render(request, 'todo_list.html', {'error': 'Usuário ou senha inválidos'})
+
+    return render(request, 'todo_list.html')
