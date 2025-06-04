2e708a7c6edec482a03d2ebd112a4c3f154b37fc
Pedro nunes <pedromarcosnunes@gmail.com>
2025-06-04 11:25:04 -0300

    04/06 3

--- projeto/setup/settings.py
+++ projeto/setup/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Configurações de Mídia
 MEDIA_URL = '/media/'
